cd422340ef809bf50148e7fcae16cf836b429eb2
Bohong Sun <u7546803@anu.edu.au>
2024-10-14 21:26:20 +1100

    Practise overload add

---
+++ calculator.cs
@@ -0,0 +1,11 @@
+class calculator
+{
+    public int Add(int a,  int b)
+    {
+        return a + b;
+    }
+
+    public double Add(double a, double b) { return a + b; }
+
+    public int Add(int a, int b, int c) { return a + b+c; }
+}
