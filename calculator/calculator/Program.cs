04aeb530edb36c9b5f794b5296292c593360e249
Bohong Sun <u7546803@anu.edu.au>
2024-10-14 21:45:27 +1100

    using the top-level statement

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public double Add(double a, double b) { return a + b; }
+
+    public int Add(int a, int b, int c) { return a + b + c; }
+}
+
+Calculator c = new Calculator();
+int sum1 = c.Add(1, 2);
+int sum2 = c.Add(2, 3 + 4);
+double sum3 = c.Add(4.2, 4.4);
+Console.WriteLine(sum1);
+Console.WriteLine(sum2);
+Console.WriteLine(sum3);
