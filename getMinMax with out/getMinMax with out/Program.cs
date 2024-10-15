2ab6ac7206a8eedc762bfc3a6bb60f6714ea7c1c
Bohong Sun <u7546803@anu.edu.au>
2024-10-15 20:17:39 +1100

    Practice how to use out

---
+++ getMinMax with out/getMinMax with out/Program.cs
@@ -0,0 +1,13 @@
+﻿void GetMinMax(int[] numbers, out int max, out int min) {
+max=numbers[0];
+    min=numbers[1];
+    foreach (int n in numbers) {
+        if (n > max) { max = n; }
+        if (n < min) { min = n; }
+    }
+}
+
+int[] numbers = { 5, 6, 7, 8, 5, 2, 2 };
+GetMinMax(numbers, out int max, out int min);
+
+Console.WriteLine($"max {max}, min {min}");
