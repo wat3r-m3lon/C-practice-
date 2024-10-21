70b6c575a618f4cc869dad8f6721cdc420971cde
Bohong Sun <u7546803@anu.edu.au>
2024-10-21 16:29:29 +1100

    Done Extension Methods

---
+++ Extension Methods/Extension Methods/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Enter You DOB");
+DateTime birthDate;
+if (DateTime.TryParse(Console.ReadLine(), out birthDate))
+{
+    int age = birthDate.CalculateAge();
+    Console.WriteLine($"You are {age}");
+}
+else
+{
+    Console.WriteLine("Invalid");
+}
+
+Console.WriteLine("Enter the date to check if it is a weekday");
+DateTime date;
+if (DateTime.TryParse(Console.ReadLine(),out date))
+{
+    bool isWeekday = date.IsWeekday();
+
+    if (isWeekday)
+    {
+        Console.WriteLine("is weekday");
+    }
+    else
+    {
+        Console.WriteLine("is not weekday");
+    }
+}
+
+
+public static class DateTimeExtensions
+{
+    public static int CalculateAge(this DateTime birthDate)
+    {
+        var today = DateTime.Today;
+        int age = today.Year - birthDate.Year;
+
+        if (birthDate.Date > today.AddYears(-age))
+        {
+            age--;
+        }
+        return age;
+    }
+
+    public static bool IsWeekday(this DateTime date)
+    {
+        DayOfWeek dayOfWeek = date.DayOfWeek;
+        return dayOfWeek >= DayOfWeek.Monday && dayOfWeek <= DayOfWeek.Friday;
+    }
+}
